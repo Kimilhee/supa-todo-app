91b3dfffe453fe0f3842d51d4627669d3cb5737c
Kimilhee <ilhee.kim@knowre.com>
2024-01-17 15:34:11 +0900

    로그 추가.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { Layout } from '../components/Layout'
 
 const Home: NextPage = () => {
-  return <Layout title="Home">hello</Layout>
+  return <Layout title="Home">hello v2</Layout>
 }
 
 export default Home
